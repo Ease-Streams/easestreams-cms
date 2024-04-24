d3f8ea2f25258700c6986c58c0b2218e0dbd2c46
mannaraees <79034666+mannaraees@users.noreply.github.com>
2024-04-24 14:34:14 +0400

    added api key to users

--- src/collections/ParentCategory.ts
+++ src/collections/ParentCategory.ts
@@ -16,26 +16,14 @@ export const ParentCategory: CollectionConfig = {
       type: 'textarea', // Field type (text for name)
       name: 'description', // Field name
       label: 'Description', // Label displayed in the admin UI
-      maxLength:200,
+      maxLength: 200,
       required: true, // Make the field mandatory
     },
     {
-        type: 'relationship', // Field type for relationships
-        name: 'rootCategoryRef',
-        label: 'Root Category', // Label displayed in the admin UI
-        relationTo: 'rootcategory',
-        admin: {
-          allowCreate: false,
-        },
-        access: {
-          update: () => false,
-        },
-      },
-    {
       type: 'relationship', // Field type for relationships
-      name: 'createdBy',
-      label: 'Created By', // Label displayed in the admin UI
-      relationTo: 'users',
+      name: 'rootCategoryRef',
+      label: 'Root Category', // Label displayed in the admin UI
+      relationTo: 'rootcategory',
       admin: {
         allowCreate: false,
       },
@@ -45,10 +33,23 @@ export const ParentCategory: CollectionConfig = {
     },
     {
       type: 'relationship', // Field type for relationships
+      name: 'createdBy',
+      label: 'Created By', // Label displayed in the admin UI
+      relationTo: 'users',
+      admin: {
+        allowCreate: false,
+      },
+      defaultValue: ({ user }) => user.id,
+      access: {
+        update: () => false,
+      },
+    },
+    {
+      type: 'relationship', // Field type for relationships
       name: 'modifiedBy',
       label: 'Modified By', // Label displayed in the admin UI
       relationTo: 'users',
-      // defaultValue: ({ user }) => user.id,
+      defaultValue: ({ user }) => user.id,
       admin: {
         allowCreate: false,
       },
@@ -70,7 +71,7 @@ export const ParentCategory: CollectionConfig = {
     {
       name: 'isDeleted',
       label: 'Deleted?',
-      type: 'checkbox', 
+      type: 'checkbox',
       defaultValue: false,
     },
   ],
--- src/collections/Products.ts
+++ src/collections/Products.ts
@@ -2,122 +2,147 @@ import type { CollectionConfig } from 'payload/types'
 import { CustomRootCategoryComponent } from '../components/fields/rootCategorySelect/component'
 
 export const Products: CollectionConfig = {
-    slug: 'products', // Collection slug (used for API endpoints)
-    admin: {
-        useAsTitle: 'name',
+  slug: 'products', // Collection slug (used for API endpoints)
+  admin: {
+    useAsTitle: 'name',
+  },
+  fields: [
+    {
+      type: 'text', // Field type (text for name)
+      name: 'name', // Field name
+      label: 'Name', // Label displayed in the admin UI
+      required: true, // Make the field mandatory
     },
-    fields: [
+    {
+      type: 'text', // Field type (text for name)
+      name: 'productCode', // Field name
+      label: 'Product Code', // Label displayed in the admin UI
+      required: true, // Make the field mandatory
+    },
+    {
+      type: 'textarea', // Field type (text for name)
+      name: 'description', // Field name
+      maxLength: 600,
+      label: 'Description', // Label displayed in the admin UI
+      required: true, // Make the field mandatory
+    },
+    {
+      name: 'productimages', // required
+      type: 'array', // required
+      label: 'Product Images',
+      required: true,
+      fields: [
         {
-            type: 'text', // Field type (text for name)
-            name: 'name', // Field name
-            label: 'Name', // Label displayed in the admin UI
-            required: true, // Make the field mandatory
+          type: 'relationship',
+          name: 'image',
+          label: 'Image',
+          relationTo: 'media',
+          required: true, // Ensure each item has a valid media relation
+        },
+      ],
+    },
+    {
+      type: 'array', // Field type (text for name)
+      name: 'videourls', // Field name
+      label: 'Video URLs', // Label displayed in the admin UI
+      fields: [
+        {
+          type: 'relationship',
+          name: 'video',
+          label: 'Video',
+          relationTo: 'media',
+          required: true, // Ensure each item has a valid media relation
+        },
+      ],
+    },
+    {
+      type: 'array', // Field type (text for name)
+      name: 'specification', // Field name
+      label: 'Specification', // Label displayed in the admin UI
+      required: false, // Make the field mandatory
+      fields: [
+        {
+          type: 'text', // Field type (text for name)
+          name: 'key',
+          label: 'Key',
         },
         {
-            type: 'text', // Field type (text for name)
-            name: 'productCode', // Field name
-            label: 'Product Code', // Label displayed in the admin UI
-            required: true, // Make the field mandatory
+          type: 'text', // Field type (text for name)
+          name: 'value',
+          label: 'Value',
         },
-        {
-            type: 'textarea', // Field type (text for name)
-            name: 'description', // Field name
-            maxLength: 600, label: 'Description', // Label displayed in the admin UI
-            required: true, // Make the field mandatory
+      ],
+    },
+    {
+      type: 'relationship', // Field type for relationships
+      name: 'rootcategoryref',
+      label: 'Root Category', // Label displayed in the admin UI
+      relationTo: 'rootcategory',
+      admin: {
+        allowCreate: false,
+        components: {
+          Field: CustomRootCategoryComponent,
         },
-        {
-            name: 'productimage', // required
-            type: 'upload', // required
-            relationTo: 'media', // required
-            required: true,
-            admin: {
-
+      },
+      access: {
+        update: () => false,
+      },
+    },
+    {
+      type: 'relationship', // Field type for relationships
+      name: 'cityref',
+      label: 'City', // Label displayed in the admin UI
+      relationTo: 'city',
+    },
+    {
+      type: 'relationship', // Field type for relationships
+      name: 'parentcategoryref',
+      label: 'Parent Category', // Label displayed in the admin UI
+      relationTo: 'parentcategory',
+    },
+    {
+      type: 'relationship', // Field type for relationships
+      name: 'createdBy',
+      label: 'Created By', // Label displayed in the admin UI
+      relationTo: 'users',
+      admin: {
+        allowCreate: false,
+      },
+      access: {
+        update: () => false,
+      },
+    },
+    {
+      type: 'relationship', // Field type for relationships
+      name: 'modifiedBy',
+      label: 'Modified By', // Label displayed in the admin UI
+      relationTo: 'users',
+      // defaultValue: ({ user }) => user.id,
+      admin: {
+        allowCreate: false,
+      },
+      access: {
+        update: () => false,
+        create: () => false,
+      },
+      hooks: {
+        afterChange: [
+          async ({ operation, req, data }) => {
+            if (operation === 'update') {
+              data.modifiedBy = req.user.id
+              return data
             }
-        },
-        {
-            type: 'text', // Field type (text for name)
-            name: 'videourl', // Field name
-            label: 'Video URL', // Label displayed in the admin UI
-            required: false, // Make the field mandatory
-            maxLength: 250
-        },
-        {
-            type: 'json', // Field type (text for name)
-            name: 'specification', // Field name
-            label: 'Specification', // Label displayed in the admin UI
-            required: false, // Make the field mandatory
-        },
-        {
-            type: 'relationship', // Field type for relationships
-            name: 'parentcategoryref',
-            label: 'Parent Category', // Label displayed in the admin UI
-            relationTo: 'parentcategory',
-        },
-        {
-            type: 'relationship', // Field type for relationships
-            name: 'rootcategoryref',
-            label: 'Root Category', // Label displayed in the admin UI
-            relationTo: 'rootcategory',
-            admin: {
-                allowCreate: false,
-                components: {
-                    Field: CustomRootCategoryComponent,
-                },
-            },
-            access: {
-                update: () => false,
-            },
-        },
-        {
-            type: 'relationship', // Field type for relationships
-            name: 'cityref',
-            label: 'City', // Label displayed in the admin UI
-            relationTo: 'city',
-        },
-
-        {
-            type: 'relationship', // Field type for relationships
-            name: 'createdBy',
-            label: 'Created By', // Label displayed in the admin UI
-            relationTo: 'users',
-            admin: {
-                allowCreate: false,
-            },
-            access: {
-                update: () => false,
-            },
-        },
-        {
-            type: 'relationship', // Field type for relationships
-            name: 'modifiedBy',
-            label: 'Modified By', // Label displayed in the admin UI
-            relationTo: 'users',
-            // defaultValue: ({ user }) => user.id,
-            admin: {
-                allowCreate: false,
-            },
-            access: {
-                update: () => false,
-                create: () => false,
-            },
-            hooks: {
-                afterChange: [
-                    async ({ operation, req, data }) => {
-                        if (operation === 'update') {
-                            data.modifiedBy = req.user.id
-                            return data
-                        }
-                    },
-                ],
-            },
-        },
-        {
-            name: 'isDeleted',
-            label: 'Deleted?',
-            type: 'checkbox',
-            defaultValue: false,
-        },
-    ],
+          },
+        ],
+      },
+    },
+    {
+      name: 'isDeleted',
+      label: 'Deleted?',
+      type: 'checkbox',
+      defaultValue: false,
+    },
+  ],
 }
 
 export default Products
