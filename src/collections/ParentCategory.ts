1bc93d643a9984dfa693ecc3c6e91a68a5ca2f54
mannaraees <79034666+mannaraees@users.noreply.github.com>
2024-09-06 15:30:10 +0400

    m

--- src/collections/ParentCategory.ts
+++ src/collections/ParentCategory.ts
@@ -1,80 +1,81 @@
-import { HTMLConverterFeature, lexicalEditor } from '@payloadcms/richtext-lexical'
-import type { CollectionConfig } from 'payload/types'
+import {
+  HTMLConverterFeature,
+  lexicalEditor,
+} from "@payloadcms/richtext-lexical";
+import type { CollectionConfig } from "payload/types";
 
 export const ParentCategory: CollectionConfig = {
-  slug: 'parentcategory', // Collection slug (used for API endpoints)
+  slug: "parentcategory", // Collection slug (used for API endpoints)
   access: {
     read: () => true,
   },
   admin: {
-    useAsTitle: 'title',
+    useAsTitle: "title",
   },
   fields: [
     {
-      type: 'text', // Field type (text for name)
-      name: 'title', // Field name
-      label: 'Category Name', // Label displayed in the admin UI
+      type: "text", // Field type (text for name)
+      name: "title", // Field name
+      label: "Category Name", // Label displayed in the admin UI
       required: true, // Make the field mandatory
     },
     {
-      type: 'textarea', // Field type (text for name)
-      name: 'description', // Field name
-      label: 'Description', // Label displayed in the admin UI
+      type: "textarea", // Field type (text for name)
+      name: "description", // Field name
+      label: "Description", // Label displayed in the admin UI
       maxLength: 200,
       required: true, // Make the field mandatory
     },
     {
-      type: 'relationship',
-      name: 'categoryImage',
-      label: 'Image',
-      relationTo: 'media',
+      type: "relationship",
+      name: "categoryImage",
+      label: "Image",
+      relationTo: "media",
       required: false, // Ensure each item has a valid media relation
     },
     {
-      type: 'relationship', // Field type for relationships
-      name: 'rootCategoryRef',
-      label: 'Root Category', // Label displayed in the admin UI
-      relationTo: 'rootcategory',
-      admin: {
-        allowCreate: false,
-      },
-      access: {
-        update: () => false,
-      },
+      type: "relationship", // Field type for relationships
+      name: "rootCategoryRef",
+      label: "Root Category", // Label displayed in the admin UI
+      relationTo: "rootcategory",
+      required: true, // Make the field mandatory
     },
 
     {
-      name: 'metaTitle',
-      type: 'text',
-      label: 'Meta Title',
+      name: "metaTitle",
+      type: "text",
+      label: "Meta Title",
     },
     {
-      name: 'metaKeyword',
-      type: 'text',
-      label: 'Meta Keywords',
+      name: "metaKeyword",
+      type: "text",
+      label: "Meta Keywords",
     },
     {
-      name: 'canonical',
-      type: 'text',
-      label: 'Canonical',
+      name: "canonical",
+      type: "text",
+      label: "Canonical",
     },
     {
-      name: 'metaDescription',
-      type: 'text',
-      label: 'Meta Description',
+      name: "metaDescription",
+      type: "text",
+      label: "Meta Description",
     },
     {
-      name: 'pageContent',
-      type: 'richText',
+      name: "pageContent",
+      type: "richText",
       editor: lexicalEditor({
-        features: ({ defaultFeatures }) => [...defaultFeatures, HTMLConverterFeature({})],
+        features: ({ defaultFeatures }) => [
+          ...defaultFeatures,
+          HTMLConverterFeature({}),
+        ],
       }),
     },
     {
-      type: 'relationship', // Field type for relationships
-      name: 'createdBy',
-      label: 'Created By', // Label displayed in the admin UI
-      relationTo: 'users',
+      type: "relationship", // Field type for relationships
+      name: "createdBy",
+      label: "Created By", // Label displayed in the admin UI
+      relationTo: "users",
       admin: {
         allowCreate: false,
       },
@@ -85,10 +86,10 @@ export const ParentCategory: CollectionConfig = {
       },
     },
     {
-      type: 'relationship', // Field type for relationships
-      name: 'modifiedBy',
-      label: 'Modified By', // Label displayed in the admin UI
-      relationTo: 'users',
+      type: "relationship", // Field type for relationships
+      name: "modifiedBy",
+      label: "Modified By", // Label displayed in the admin UI
+      relationTo: "users",
       defaultValue: ({ user }) => user.id,
       admin: {
         allowCreate: false,
@@ -100,21 +101,21 @@ export const ParentCategory: CollectionConfig = {
       hooks: {
         afterChange: [
           async ({ operation, req, data }) => {
-            if (operation === 'update') {
-              data.modifiedBy = req.user.id
-              return data
+            if (operation === "update") {
+              data.modifiedBy = req.user.id;
+              return data;
             }
           },
         ],
       },
     },
     {
-      name: 'isDeleted',
-      label: 'Deleted?',
-      type: 'checkbox',
+      name: "isDeleted",
+      label: "Deleted?",
+      type: "checkbox",
       defaultValue: false,
     },
   ],
-}
+};
 
-export default ParentCategory
+export default ParentCategory;
